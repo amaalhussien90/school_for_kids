d72756f4ee6bb13327bbcd295aeb5d58b7a54dfd
Amaal Hussien <moas178954@gmail.com>
2020-05-08 02:24:18 +0300

    complate all page

---
+++ layout/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    'use strict';
+    // Adjust Slider Height
+    var winH = $(window).height(),
+        upperH = $('.upper-bar').innerHeight(),
+        navH = $('.navbar').innerHeight();
+    $('.slider, .carousel-item').height(winH - navH);
+
+
+});
